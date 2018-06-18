d96996d0b95c4595f4aa7b3c79ae59cc0abb4e7e
Vincent Houba <edil3ra@gmail.com>
2018-06-18 10:44:06 +0200

    Remove depedencies in typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,28 +1,8 @@
 import Typography from 'typography'
-import grandViewTheme from 'typography-theme-grand-view'
 import bootstrapTheme from "typography-theme-bootstrap";
-import lawtonTheme from "typography-theme-lawton";
 
 
-
-// const typography = new Typography(grandViewTheme);
 const typography = new Typography(bootstrapTheme);
-// const typography = new Typography(lawtonTheme)
-
-
-// const typography = new Typography({
-//   baseFontSize: "30px",
-//   baseLineHeight: 1.45,
-//   headerFontFamily: [
-//     "Avenir Next",
-//     "Helvetica Neue",
-//     "Segoe UI",
-//     "Helvetica",
-//     "Arial",
-//     "sans-serif",
-//   ],
-//   bodyFontFamily: ["Georgia", "serif"],
-// });
 
 
 export default typography
